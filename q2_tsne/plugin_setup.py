8e5f1ebf7fbd5b3882399c177bde7d2329a7df4d
azhang30 <63296197+azhang30@users.noreply.github.com>
2020-12-05 17:30:14 -0500

    Update plugin_setup.py

--- q2_tsne/plugin_setup.py
+++ q2_tsne/plugin_setup.py
@@ -5,19 +5,19 @@ from q2_types.distance_matrix import DistanceMatrix
 from q2_types.ordination import PCoAResults
 from q2_diversity import _beta as beta
 import q2_tsne
-from q2_tsne._tsne import (_joint_probabilities, _kl_divergence, _kl_divergence_bh, _tsne)
+from q2_tsne._t_sne import (_joint_probabilities, _kl_divergence, _kl_divergence_bh, __init__)
 
-plugin = Plugin(
+plugin = qiime2.plugin.Plugin(
         name='tsne',
         version='0.0.1',
-        website='https://github.com/azhang30/q2-tsne/blob/main/q2_tsne/plugin_setup.py',
+        website='github.com/scikit-learn/scikit-learn/blob/0fb307bf3/sklearn/manifold/_t_sne.py#L476',
         package='q2_tsne',
         description=('A t-SNE QIIME2 plugin for Adam Zhangs Final Project Fall 2020'),
         short_description='t-SNE QIIME2 plugin',
         )
 
 plugin.methods.register_function(
-        function=joint_probabilities,
+        function=_joint_probabilities,
         inputs={'X': DistanceMatrix},
         parameters={
             'distances': DistanceMatrix,
@@ -26,10 +26,21 @@ plugin.methods.register_function(
             },
         outputs=[('P', DistanceMatrix),
         ],
+        input_descriptions={
+            'X': ('Distance Matrix output from QIIME 2 beta diversity analysis on which PCoA is computed')
+            },
+        parameter_descriptions={
+            'distances': ('Distances of samples stored as one-dimensional matrices'),
+            'desired_perplexity': ('Desired perplexity of joint probability distributions, default 30'),
+            'verbose': ('Verbosity level, default 0')
+            },
+        output_descriptions={'P': 'Condensed joint probability matrix'},
+        name='Joint Probabilities',
+        description=('Compute joint probabilities p_ij from distances.')
 )
 
 plugin.methods.register_function(
-        function=kl_divergence,
+        function=_kl_divergence,
         inputs={'X': DistanceMatrix},
         parameters={
             'params': DistanceMatrix,
@@ -44,10 +55,26 @@ plugin.methods.register_function(
         outputs=[('kl_divergence', Float),
                  ('grad', DistanceMatrix)
         ],
+        input_descriptions={
+            'X': ('Distance Matrix output from QIIME 2 beta diversity analysis on which PCoA is computed')
+            },
+        parameter_descriptions={
+            'params': ('Unraveled embedding'),
+            'P': ('Condensed joint probability matrix'),
+            'degrees_of_freedom': ('Student t-dist DoF'),
+            'n_samples': ('Number of samples'),
+            'n_components': ('Dimension of embedded space'),
+            'skip_num_points': ('Gradient is not computed for points with indices below this threshold'),
+            'compute_error': ('kl_divergence not computed if False')
+            },
+        output_descriptions={'kl_divergence': 'Kullback-Leibler divergence of p_ij and q_ij',
+                             'grad': 'Unraveled gradient of the KL divergence with respect to embedding'},
+        name='Kullback-Leibler divergence',
+        description='t-SNE objective function: gradient of the KL divergence of p_ijs and q_ijs with absolute error'
 )
 
 plugin.methods.register_function(
-        function=kl_divergence_bh,
+        function=_kl_divergence_bh,
         inputs={'X': DistanceMatrix},
         parameters={
             'params': DistanceMatrix,
@@ -64,10 +91,29 @@ plugin.methods.register_function(
         outputs=[('kl_divergence', Float),
                  ('grad', DistanceMatrix)
         ],
+        input_descriptions={
+            'X': ('Distance Matrix output from QIIME 2 beta diversity analysis on which PCoA is computed')
+            },
+        parameter_descriptions={
+            'params': ('Unraveled embedding'),
+            'P': ('Condensed joint probability matrix'),
+            'degrees_of_freedom': ('Student t-dist DoF'),
+            'n_samples': ('Number of samples'),
+            'n_components': ('Dimension of embedded space'),
+            'angle': ('Angular size used in Barnes-Hut t-SNE'),
+            'skip_num_points': ('Gradient is not computed for points with indices below this threshold'),
+            'verbose': ('Verbosity level, default 0'),
+            'compute_error': ('kl_divergence not computed if False'),
+            'num_threads': ('Number of threads used to compute the gradient.')
+            },
+        output_descriptions={'kl_divergence': 'Kullback-Leibler divergence of p_ij and q_ij',
+                             'grad': 'Unraveled gradient of the KL divergence with respect to embedding'},
+        name='Barnes-Hut Kullback-Leibler divergence',
+        description='t-SNE objective function: gradient of the KL divergence of p_ijs and q_ijs using Barnes-Hut tree methods to calculate the gradient that runs in 0(NlogN) instead of 0(N^2)'
 )
 
 plugin.methods.register_function(
-        function=tsne,
+        function=__init__,
         inputs={'X': DistanceMatrix},
         parameters={
             'n_components': Int,
@@ -88,9 +134,27 @@ plugin.methods.register_function(
         outputs=[('pcoa', PCoAResults),
         ],
         input_descriptions={
-            'distance_matrix': ('The distance matrix on which PCoA should be computed.')
+            'X': ('Distance Matrix output from QIIME 2 beta diversity analysis on which PCoA is computed')
             },
-        output_descriptions={'pcoa': 'The resulting PCoA matrix.'}
+        parameter_descriptions={
+            'n_components': ('Dimension of the embedded space, default 2'),
+            'perplexity': ('Number of nearest neighbors that is used in other manifold learning algorithms, default 30'),
+            'early_exaggeration': ('Controls how tight natural clusters in the original space are in the embedded space and how much space will be between them, default 12'),
+            'learning_rate': ('The learning rate, default 200'),
+            'n_iter': ('Max number of iterations for optimization, default 1000'),
+            'n_iter_without_progress': ('Max number of iterations without progress before abort, default 300'),
+            'min_grad_norm': ('Optimization is stopped if gradient norm falls below threshold, default 1e-7'),
+            'metric': ('Metric to use when calculating distance between instances in a feature array, default is assumed to be distance matrix'),
+            'init': ('Initialization of embedding, default random'),
+            'verbose': ('Verbosity level, default 0'),
+            'random_state': ('Determine the random number generator, default None'),
+            'method': ('Gradient calculation algorithm, default is Barnes-Hut'),
+            'angle': ('Angular sized used with Barnes-Hut, default 0.5'),
+            'n_jobs': ('Number of parallel jobs to run for neighbors search')
+            },
+        output_descriptions={'pcoa': 'The resulting PCoA matrix.'},
+        name='t-SNE',
+        description='t-SNE is a tool to visualize high-dimensional data. It converts similarities between data points to joint probabilities and tries to minimize the Kullback-Leibler divergence between the joint probabilities of the low-dimensional embedding and the high-dimensional data.'
 )
 
 
